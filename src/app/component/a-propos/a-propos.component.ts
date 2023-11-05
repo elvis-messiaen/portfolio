1c183e7650e1df33063197018776c4b19e2e01d7
MESSIAEN Elvis <elvism72@gmail.com>
2023-11-05 13:45:31 +0100

    Mise en place de la page a propos

--- src/app/component/a-propos/a-propos.component.ts
+++ src/app/component/a-propos/a-propos.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-a-propos',
   templateUrl: './a-propos.component.html',
   styleUrls: ['./a-propos.component.scss']
 })
-export class AProposComponent {
+export class AProposComponent implements OnInit{
 
+    constructor(private router: Router) {}
+  ngOnInit(): void {
+  }
+
+  redirect() {
+      this.router.navigate(['/contact'])
+  }
 }
